3a6bca1351e6ac7b30d1fcf3cd92cfb1dc308967
Ella Davis <qcg503@york.ac.uk>
2025-05-23 21:44:12 +0100

    Added Convert to Rasterio Function

--- terrain_analysis.py
+++ terrain_analysis.py
@@ -5,3 +5,11 @@ import geopandas as gpd
 import numpy as np
 from rasterio.mask import mask
 from rasterio.features import geometry_mask
+
+# A function which opens up the raster files and reads in the data, location and metadata.
+def convert_to_rasterio(raster_data_path, template_raster_path = None):
+    with rasterio.open(raster_data_path) as src:
+        data = src.read(1)
+        profile = src.profile
+        transform = src.transform
+    return data, transform, profile
