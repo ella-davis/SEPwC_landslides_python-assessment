8655c0b859f7a5e87a9f9dde148c3c10cd4dbc27
Ella Davis <qcg503@york.ac.uk>
2025-05-23 21:53:47 +0100

    Added Core Example from Skeleton Script

--- terrain_analysis.py
+++ terrain_analysis.py
@@ -19,3 +19,38 @@ def extract_values_from_raster(raster_path, shape_object):
     with rasterio.open(raster_path) as src:
         out_image, out_transform = mask(src, shape_object.geometry, crop = True)
         return out_image[0] 
+
+# The core function
+def main():
+
+    parser = argparse.ArgumentParser(
+                     prog="Landslide hazard using ML",
+                     description="Calculate landslide hazards using simple ML",
+                     epilog="Copyright 2024, Jon Hill"
+                     )
+    parser.add_argument('--topography',
+                    required=True,
+                    help="topographic raster file")
+    parser.add_argument('--geology',
+                    required=True,
+                    help="geology raster file")
+    parser.add_argument('--landcover',
+                    required=True,
+                    help="landcover raster file")
+    parser.add_argument('--faults',
+                    required=True,
+                    help="fault location shapefile")
+    parser.add_argument("landslides",
+                    help="the landslide location shapefile")
+    parser.add_argument("output",
+                    help="the output raster file")
+    parser.add_argument('-v', '--verbose',
+                    action='store_true',
+                    default=False,
+                    help="Print progress")
+
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
